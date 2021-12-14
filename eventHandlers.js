26d7cbb7bbfa99bb2cabb9d8dfeffe9271910c27
thouefab <thouefab@students.zhaw.ch>
2021-12-14 17:40:33 +0100

    Erde

--- eventHandlers.js
+++ eventHandlers.js
@@ -5,10 +5,66 @@ exports.sendEvent = null;
 exports.registerEventHandlers = function (source) {
     source.addEventListener('MyEvent', handleMyEvent);
     // Register more event handlers here
+    source.addEventListener('Erde', handleErde);
+    source.addEventListener('Lux', handleLux);
     
 }
 
 function handleMyEvent(event) {
+    // read variables from the event
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    //var datetime = new Date(data.timestamp); // convert the timestamp to a Date object
+
+    try {        
+        // you can add more properties to your data object
+        data.myMessage = "Hello WorldFabian";
+
+        // TODO: do something meaningful with the data
+
+        // Log the event in the database
+        logger.logOne("MyDB", "MyEvent", data);
+
+        // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+function handleErde(event) {
+    // read variables from the event
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    //var datetime = new Date(data.timestamp); // convert the timestamp to a Date object
+
+    try {        
+        // you can add more properties to your data object
+        data.myMessage = "Fabian";
+
+        // TODO: do something meaningful with the data
+
+        // Log the event in the database
+        logger.logOne("MyDB", "Erde", data);
+
+        // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+function handleLux(event) {
     // read variables from the event
     var data = {
         eventName: event.type,
